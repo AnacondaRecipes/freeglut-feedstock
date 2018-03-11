f6a0e0fd055eeeca1994fefc2657fa4c6455f665
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-03-11 07:00:35 -0500

    Make recipe cross compile capable; CB3ize

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+set -x
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*linux.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
 
 mkdir build && cd build
 cmake \
 	-DCMAKE_INSTALL_PREFIX=$PREFIX          \
 	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib      \
 	-DCMAKE_BUILD_TYPE=Release              \
+        ${CMAKE_PLATFORM_FLAGS[@]}              \
 	-DFREEGLUT_BUILD_DEMOS=OFF              \
 	..
 make
