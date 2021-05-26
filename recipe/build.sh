0a1afa7f4edce7fa647863afb9d5e90699e74998
Serhii Kupriienko <skupriienko@anaconda.com>
2021-05-26 13:53:12 +0300

    Add patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,18 @@
+#!/bin/bash
+set -x
+
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*linux.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 mkdir build && cd build
-cmake ^
-	-G "NMake Makefiles"                     ^
-	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
-	-DCMAKE_INSTALL_BINDIR=%LIBRARY_BIN%     ^
-	-DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB%     ^
-	-DCMAKE_INSTALL_INCLUDEDIR=%LIBRARY_INC% ^
-	-DCMAKE_BUILD_TYPE=Release               ^
-	-DFREEGLUT_REPLACE_GLUT=ON               ^
-	-DFREEGLUT_BUILD_DEMOS=OFF               ^
-	-DFREEGLUT_BUILD_STATIC_LIBS=OFF         ^
-	-DFREEGLUT_BUILD_SHARED_LIBS=ON          ^
+cmake \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX          \
+	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib      \
+	-DCMAKE_BUILD_TYPE=Release              \
+        ${CMAKE_PLATFORM_FLAGS[@]}              \
+	-DFREEGLUT_BUILD_DEMOS=OFF              \
 	..
-
-cmake --build . --config Release --target INSTALL
-
-mklink /h %LIBRARY_BIN%\freeglut.dll %LIBRARY_BIN%\glut.dll
-mklink /h %LIBRARY_LIB%\freeglut.lib %LIBRARY_LIB%\glut.lib
+make
+make install
