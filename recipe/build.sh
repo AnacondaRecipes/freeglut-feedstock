d80b6e4c436ce4a59d2ea3e3d899340e7e303590
Maxwell Varlack <varlackc@mail.gvsu.edu>
2021-05-26 22:40:33 +0000

    Correct current issues on graviton

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,13 +6,16 @@ if [[ ${HOST} =~ .*linux.* ]]; then
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
+export LDFLAGS="${LDFLAGS} -Wl,--allow-multiple-definition"
+
 mkdir build && cd build
 cmake \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX          \
-	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib      \
-	-DCMAKE_BUILD_TYPE=Release              \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX          \
+        -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib      \
+        -DCMAKE_BUILD_TYPE=Release              \
         ${CMAKE_PLATFORM_FLAGS[@]}              \
-	-DFREEGLUT_BUILD_DEMOS=OFF              \
+        -DFREEGLUT_BUILD_DEMOS=OFF              \
 	..
+
 make
 make install
