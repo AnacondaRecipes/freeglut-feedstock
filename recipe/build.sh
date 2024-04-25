2e88af163bcdf909a14d2e7cf3cdd789da8e7175
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-25 14:26:18 +0300

    freeglut 3.2.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-set -x
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*linux.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
 
 mkdir build && cd build
 cmake \
 	-DCMAKE_INSTALL_PREFIX=$PREFIX          \
 	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib      \
 	-DCMAKE_BUILD_TYPE=Release              \
-        ${CMAKE_PLATFORM_FLAGS[@]}              \
 	-DFREEGLUT_BUILD_DEMOS=OFF              \
+	-DFREEGLUT_BUILD_STATIC_LIBS=OFF        \
+	-DFREEGLUT_BUILD_SHARED_LIBS=ON         \
 	..
+
 make
 make install
