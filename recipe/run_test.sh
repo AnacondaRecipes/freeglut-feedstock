18ada027b63d7ce8977f97a0e84624e4f3813412
Serhii Kupriienko <skupriienko@anaconda.com>
2024-05-10 15:24:09 +0300

    Move test commands to run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Test includes.
+test -d "${PREFIX}/include/GL"
+
+	# Test libraries.
+test ! -f "${PREFIX}/lib/libglut.a"
+test -f "${PREFIX}/lib/libglut.so"
+ldd "${PREFIX}/lib/libglut.so"
+
 # Test linking against glut with CMake
-cd test
+cd test || exit
 
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release .
 
-cmake --build . --config Release
+ninja -j${CPU_COUNT}
+ninja install
 
 ./test
